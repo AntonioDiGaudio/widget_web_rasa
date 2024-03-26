222545a0ddbdf5c4eb54b835eeb496e599ca4c6b
AntonioDiGaudio <antoniodigaudio16@gmail.com>
2024-03-26 17:39:42 +0100

    Update all

--- Rasa_widget_web/Project Rasa/actions/actions.py
+++ Rasa_widget_web/Project Rasa/actions/actions.py
@@ -7,21 +7,88 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class SalutoActionClass(Action):
+
+    def name(self) -> Text:
+        return "act_saluto"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Ciao compare, la classe action ti saluta")
+
+        return []
+
+
+
+
+
+
+class TripCarousel(Action):
+
+    def name(self) -> Text:
+        return "act_city_list"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+
+        new_carousel = {
+            "type" : "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [
+                    {
+                        "title": "Info",
+                        "subtitle": "Scopri di più",
+                        "image_url": "https://travelino.it/wp-content/uploads/2024/03/info-1.png",
+                        "buttons" : [
+                            {
+
+                                "title": "Clicca qui",
+                                "url": "https://travelino.it/#news",
+                                "type": "web_url",
+                            },  
+                        ]
+                    },
+                     {
+                        "title": "Chiamaci",
+                        "subtitle": "Connettiti con noi al volo",
+                        "image_url": "https://travelino.it/wp-content/uploads/2024/03/Chiamaci.png",
+                        "buttons" : [
+                            {
+
+                                "title": "Clicca qui",
+                                "url": "tel:0919767677",
+                                "type": "web_url",
+                            },
+                            
+                 ]
+            },
+            {
+                        "title": "Fissa un appuntamento",
+                        "subtitle": "Conosci il tuo team",
+                        "image_url": "https://travelino.it/wp-content/uploads/2024/03/Fissa-un-appuntamento.png",
+                        "buttons" : [
+                            {
+
+                                "title": "Clicca qui",
+                                "url": "https://calendly.com/travelino ",
+                                "type": "web_url",
+                            },  
+                        ]
+                    },
+            ]
+        }
+    }    
+
+
+        dispatcher.utter_message(text="Ciao, sono Travelino 😎, chiedimi ciò che vuoi oppure clicca una delle opzioni di seguito: " , attachment = new_carousel)
+
+        return []
